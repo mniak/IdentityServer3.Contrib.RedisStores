b3fd29f64eb6193b9131057067927874203a3381
Andre Guilherme <andregsilv@gmail.com>
2016-10-28 10:35:51 -0200

    Add unit test project

---
+++ test/IdentityServer3.Contrib.RedisStores.Tests/TokenHandleStoreTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace IdentityServer3.Contrib.RedisStores.Tests
+{
+    public class TokenHandleStoreTests
+    {
+        [Fact]
+        public void PassingTest()
+        {
+        }
+    }
+}
