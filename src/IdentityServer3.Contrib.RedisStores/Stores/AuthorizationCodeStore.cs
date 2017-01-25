8189bbd57510645605d1def6dff45d92b28b9761
Andre Guilherme <andregsilv@gmail.com>
2017-01-25 08:52:02 -0200

    Add IClientStore

--- src/IdentityServer3.Contrib.RedisStores/Stores/AuthorizationCodeStore.cs
+++ src/IdentityServer3.Contrib.RedisStores/Stores/AuthorizationCodeStore.cs
@@ -9,19 +9,23 @@ namespace IdentityServer3.Contrib.RedisStores
     public sealed class AuthorizationCodeStore : RedisTransientStore<AuthorizationCode>, IAuthorizationCodeStore
     {
         private readonly IScopeStore scopeStore;
+        private readonly IClientStore clientStore;
 
-        public AuthorizationCodeStore(IDatabase redis, RedisOptions options, IScopeStore scopeStore) : base(redis)
+        public AuthorizationCodeStore(IDatabase redis, RedisOptions options, IScopeStore scopeStore, IClientStore clientStore) : base(redis)
         {
             this.scopeStore = scopeStore;
+            this.clientStore = clientStore;
         }
-        public AuthorizationCodeStore(IDatabase redis, IScopeStore scopeStore) : base(redis)
+        public AuthorizationCodeStore(IDatabase redis, IScopeStore scopeStore, IClientStore clientStore) : base(redis)
         {
             this.scopeStore = scopeStore;
+            this.clientStore = clientStore;
         }
         protected override void CustomizeSerializerSettings(JsonSerializerSettings settings)
         {
             base.CustomizeSerializerSettings(settings);
             settings.Converters.Add(new ScopeConverter(scopeStore));
+            settings.Converters.Add(new AuthorizationCodeConverter(clientStore));
         }
         internal override string CollectionName => "auth_codes";
         internal override int GetTokenLifetime(AuthorizationCode token)
