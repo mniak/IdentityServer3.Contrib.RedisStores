126811b0862fbcb6769a832419dde6b18d03f648
Andre Guilherme <andregsilv@gmail.com>
2017-01-25 08:56:43 -0200

    Add collection handling capabilities

--- src/IdentityServer3.Contrib.RedisStores/Serialization/ScopeConverter.cs
+++ src/IdentityServer3.Contrib.RedisStores/Serialization/ScopeConverter.cs
@@ -2,6 +2,7 @@
 using IdentityServer3.Core.Services;
 using Newtonsoft.Json;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace IdentityServer3.Contrib.RedisStores.Serialization
@@ -16,20 +17,45 @@ namespace IdentityServer3.Contrib.RedisStores.Serialization
         }
         public override bool CanConvert(Type objectType)
         {
-            return objectType == typeof(Scope);
+            return objectType == typeof(Scope)
+                || objectType == typeof(List<Scope>)
+                || objectType == typeof(Scope[])
+                || objectType == typeof(IEnumerable<Scope>);
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
-            var scopeName = serializer.Deserialize<string>(reader);
-            var scope = scopeStore.FindScopesAsync(new[] { scopeName }).Result.Single();
-            return scope;
+            if (objectType == typeof(Scope))
+            {
+                var scopeName = serializer.Deserialize<string>(reader);
+                var scope = scopeStore.FindScopesAsync(new[] { scopeName }).Result.Single();
+                return scope;
+            }
+            else
+            {
+                var scopeNames = serializer.Deserialize<IEnumerable<string>>(reader);
+                var scopes = scopeStore.FindScopesAsync(scopeNames).Result;
+                if (objectType == typeof(List<Scope>))
+                    return scopes.ToList();
+                else if (objectType == typeof(Scope[]))
+                    return scopes.ToArray();
+                return scopes;
+            }
         }
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            var scope = value as Scope;
-            serializer.Serialize(writer, scope.Name);
+            var scopesCollection = value as IEnumerable<Scope>;
+            if (scopesCollection != null)
+            {
+                var scopeNames = scopesCollection.Select(x => x.Name);
+                serializer.Serialize(writer, scopeNames);
+            }
+            else
+            {
+                var scope = value as Scope;
+                serializer.Serialize(writer, scope.Name);
+            }
         }
     }
 }
