23059fe28dcee0a28977c46bf0b5b6139aa2a409
Andre Guilherme <andregsilv@gmail.com>
2017-01-25 08:54:07 -0200

    Add constructor that accepts ClaimsPrincipal and change the list of ClaimModels with IEnumerable<Claim>

--- src/IdentityServer3.Contrib.RedisStores/Models/ClaimsPrincipalModel.cs
+++ src/IdentityServer3.Contrib.RedisStores/Models/ClaimsPrincipalModel.cs
@@ -1,13 +1,21 @@
 ﻿using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
 
 namespace IdentityServer3.Contrib.RedisStores.Models
 {
     internal class ClaimsPrincipalModel
     {
+        public ClaimsPrincipalModel() { }
+        public ClaimsPrincipalModel(ClaimsPrincipal claimsPrincipal)
+        {
+            this.AuthenticationType = claimsPrincipal.Identity.AuthenticationType;
+            this.Claims = claimsPrincipal.Claims;
+        }
         [JsonProperty("amr")]
         public string AuthenticationType { get; set; }
         [JsonProperty("claims")]
-        public List<Models.ClaimModel> Claims { get; set; }
+        public IEnumerable<Claim> Claims { get; set; }
     }
 }
