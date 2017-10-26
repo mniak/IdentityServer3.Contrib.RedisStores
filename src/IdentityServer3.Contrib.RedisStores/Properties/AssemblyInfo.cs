2fcfb62db28ca0a8198bde2c447e600aa9f26b25
Andre Guilherme <andre.guilherme@iatec.com>
2017-10-26 09:38:42 -0200

    Upgrade project file

--- src/IdentityServer3.Contrib.RedisStores/Properties/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("IdentityServer3.Contrib.RedisStores")]
-[assembly: AssemblyTrademark("")]
